b0d8df62e3a410faa3056faad40ed5c587ec8acc
CYBORG-ED <gabrieldvincent0100@outlook.com>
2022-09-21 11:01:09 +0000

    11:00

--- Simple.py
+++ Simple.py
@@ -20,18 +20,37 @@ def divide(x, y):
     return x / y
 
 
+# This function modulus two numbers
+def modulus(x, y):
+    return x % y
+
+
+# This function divides two numbers
+def exponentiation(x, y):
+    return x ** y
+
+
+# This function divides two numbers
+def floor_division(x, y):
+    return x // y
+
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.modulus")
+print("6.exponentiation")
+print("7.floor_division")
+
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5/6/7): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '5', '6', '7'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -46,6 +65,16 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+
+        elif choice == '5':
+            print(num1, "%", num2, "=", modulus(num1, num2))
+
+        elif choice == '6':
+            print(num1, "**", num2, "=",	exponentiation(num1, num2))
+
+
+        elif choice == '7':
+            print(num1, "//", num2, "=", floor_division(num1, num2))
 
         # check if user wants another calculation
         # break the while loop if answer is no
